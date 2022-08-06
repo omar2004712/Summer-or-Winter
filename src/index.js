5fedfdb705e4eb087ffa4a10e3b8bcd974470996
omar2004712 <o.bahutar@gmail.com>
2022-08-06 11:04:30 +0400

    refactor(index): refactored the loading of the location to the life cycle method 'componentDidMount' instead of the constructor

--- src/index.js
+++ src/index.js
@@ -3,13 +3,45 @@ import ReactDOM from 'react-dom/client';
 
 // eslint-disable-next-line react/prefer-stateless-function
 class App extends React.Component {
-  render() {
-    window.navigator.geolocation.getCurrentPosition(
-      (position) => console.log(position),
-      (err) => console.log(err)
-    );
+  constructor(props) {
+    super(props);
 
-    return <div>Latitude: </div>;
+    this.state = { lat: null, errorMessage: '', date: null };
+  }
+
+  componentDidMount() {
+    // runs after the component rendered for the first time
+    // runs only once
+    window.navigator.geolocation.getCurrentPosition(
+      ({ coords: { latitude } }) => {
+        // ====== to update the lat state we used setState ==========
+        this.setState({ lat: latitude });
+
+        // do not use
+        // this.state.lat = latitude
+      },
+      (err) => {
+        this.setState({ errorMessage: err.message });
+      }
+    );
+  }
+
+  componentDidUpdate() {
+    // runs after the component rerenders -update-
+    // runs as many times as the components updates
+    console.log('My Component was just updated - it rerendered!');
+  }
+
+  // render function Required By React
+  render() {
+    const { lat, errorMessage } = this.state;
+    if (errorMessage && !lat) {
+      return <div>Error: {errorMessage}</div>;
+    }
+    if (!errorMessage && lat) {
+      return <div>Latitude: {lat}</div>;
+    }
+    return <div>Loading...</div>;
   }
 }
 
